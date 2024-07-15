6a4b91e6037205f16cd4aa5bd0a9779f1c1504e5
v <v>
2024-07-15 16:45:39 +0800

    更新linux安装脚本

--- install_linux.sh
+++ install_linux.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-proj_path="$(dirname "$0")"
-proj_path="$(realpath -e "$proj_path")"
+proj_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 install_by_path() {
   mkdir -p "$(dirname "$1")"
-  #ln -s "$proj_path" "$1"
-  #echo "已创建软链接: $1 -> $proj_path"
   cp -frT "$proj_path" "$1"
   echo "已复制: $proj_path 到 $1"
 }
@@ -16,7 +13,14 @@ install_for_fcitx5() {
   install_by_path "$HOME/.local/share/fcitx5/rime"
 }
 
-read -p "请选择你使用的输入法的序号: (1)ibus (2)fcitx5. 默认: 全部: " method
+if [ "$1" == "ibus" ]; then
+  method=1
+elif [ "$1" == "fcitx5" ]; then
+  method=2
+else
+  read -p "请选择你使用的输入法的序号: (1)ibus (2)fcitx5. 默认: 全部: " method
+fi
+
 case $method in
 1) echo
     install_for_ibus
